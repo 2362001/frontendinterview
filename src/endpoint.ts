54ab46241f1b26977b88d6ac2d9a39d0ee49e1b5
2362001 <o0othanh2k1@gmail.com>
2024-03-03 10:55:53 +0700

    update endpoint

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -1,16 +1,16 @@
-const ENDPOINT_GETALLDATA = 'http://localhost:2001/api/v1/userInfor/getAll'
-const ENDPOINT_SEARCHDATA = 'http://localhost:2001/api/v1/userInfor/searchByCustomerCode'
-const ENDPOINT_CREATEDATA = 'http://localhost:2001/api/v1/userInfor/createUserInfo'
+const ENDPOINT_GETALLDATA = 'https://beinterview.vercel.app/api/v1/userInfor/getAll'
+const ENDPOINT_SEARCHDATA = 'https://beinterview.vercel.app/api/v1/userInfor/searchByCustomerCode'
+const ENDPOINT_CREATEDATA = 'https://beinterview.vercel.app/api/v1/userInfor/createUserInfo'
 
-const ENDPOINT_CREATEPRODUCT = 'http://localhost:2001/api/v1/product/createProduct'
-const ENDPOINT_GETALLPRODUCT = 'http://localhost:2001/api/v1/product/getAllProduct'
-const ENDPOINT_GETTOTALCOUNTPRODUCT = 'http://localhost:2001/api/v1/product/getSumProduct'
-const ENDPOINT_GETBYCUSTOMERCODE = 'http://localhost:2001/api/v1/product/getByCustomerCode'
+const ENDPOINT_CREATEPRODUCT = 'https://beinterview.vercel.app/api/v1/product/createProduct'
+const ENDPOINT_GETALLPRODUCT = 'https://beinterview.vercel.app/api/v1/product/getAllProduct'
+const ENDPOINT_GETTOTALCOUNTPRODUCT = 'https://beinterview.vercel.app/api/v1/product/getSumProduct'
+const ENDPOINT_GETBYCUSTOMERCODE = 'https://beinterview.vercel.app/api/v1/product/getByCustomerCode'
 
-const ENDPOINT_CREATEPRODUCTPURCHASE = 'http://localhost:2001/api/v1/productpurchase/createProductPurchase'
-const ENDPOINT_GETALLPRODUCTPURCHASE = 'http://localhost:2001/api/v1/productpurchase/getAllProductPurchase'
-const ENDPOINT_GETBYCUSTOMERCODEPURCHASE = 'http://localhost:2001/api/v1/productpurchase/getByCustomerCodePurchase'
-const ENDPOINT_GETTOTALCOUNTPRODUCTPURCHASE = 'http://localhost:2001/api/v1/productpurchase/getSumProductPurchase'
+const ENDPOINT_CREATEPRODUCTPURCHASE = 'https://beinterview.vercel.app/api/v1/productpurchase/createProductPurchase'
+const ENDPOINT_GETALLPRODUCTPURCHASE = 'https://beinterview.vercel.app/api/v1/productpurchase/getAllProductPurchase'
+const ENDPOINT_GETBYCUSTOMERCODEPURCHASE = 'https://beinterview.vercel.app/api/v1/productpurchase/getByCustomerCodePurchase'
+const ENDPOINT_GETTOTALCOUNTPRODUCTPURCHASE = 'https://beinterview.vercel.app/api/v1/productpurchase/getSumProductPurchase'
 
 export {
   ENDPOINT_GETALLDATA,
